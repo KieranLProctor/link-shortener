5a9ba486b674b11f0bad37e2abca872e4717a23f
KieranProctorMVH <kieranp@mvhltd.com>
2022-06-28 16:59:40 +0100

    added report controller as ivokable

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\LinkController;
 use App\Http\Controllers\LinkInfoController;
+use App\Http\Controllers\LinkReportController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -37,6 +38,7 @@ Route::middleware([
 
     Route::resource('/links', LinkController::class);
     Route::get('/links/{link}/info', LinkInfoController::class)->name('links.info');
+    Route::get('/links/{link}/report', LinkReportController::class)->name('links.report');
 
     // Route::get('/links/{link}/report', [LinkReportController::class, 'create'])->name('link-reports.create');
     // Route::post('/links/{link}/report', [LinkReportController::class, 'store'])->name('link-reports.store');
